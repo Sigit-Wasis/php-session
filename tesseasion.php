5dd7dd1d5438174519dbe487f5c3f5c23b3e2211
sigitwasissubekti <sigitghoticmetal2001@gmail.com>
2018-08-10 13:23:19 +0700

    First Commit

---
+++ tesseasion.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tes session</title>
+</head>
+<body>
+<?php
+    session_start();
+    if (isset($_SESSION["nama"])) 
+        echo "session sudah aktif dengan isi : ".$_SESSION["nama"];
+    else   
+        echo "session belum aktif atau sudah tidak aktif lagi";
+    
+?>
+</body>
+</html>
