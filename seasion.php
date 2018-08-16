7929dd52d66a573fa6aa1b45ed0f3c1aaae59982
sigitwasissubekti <sigitghoticmetal2001@gmail.com>
2018-08-16 17:27:08 +0700

    second commit

--- seasion.php
+++ seasion.php
@@ -11,7 +11,7 @@
 
 session_start();
 $_SESSION["nama"]="sigit";
-
+                                                                                                                                                  
 ?>
 </body>
 </html>
